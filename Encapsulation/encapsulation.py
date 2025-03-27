29ffed2121a224da9905582c65733052c1827862
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-03-27 10:22:03 +0000

    Encapsulation

---
+++ Encapsulation/encapsulation.py
@@ -0,0 +1,20 @@
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.__balance = balance
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def withdraw(self, amount):
+        if amount <= self.__balance:
+            self.__balance -= amount
+        else:
+            print("Insufficient funds")
+
+    def get_balance(self):
+        return self.__balance
+
+account = BankAccount("Alice", 1000)
+account.deposit(500)
+print(account.get_balance())
